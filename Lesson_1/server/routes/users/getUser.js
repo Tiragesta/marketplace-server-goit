d7487a2abffeb42c943d4b7f40316588d80bcd5f
Tiragesta <eugenrashupkina@gmail.com>
2018-10-16 12:49:17 +0300

    add new file

--- Lesson_1/server/routes/users/getUser.js
+++ Lesson_1/server/routes/users/getUser.js
@@ -1,22 +1,18 @@
-const url = require('url');
-
-const getId = url => {
-  const lastIndex = url.lastIndexOf('/');
-
-  if (lastIndex !== -1) {
-    return url.slice(lastIndex +1);
-  }
-};
+const userBase = require('./userBase');
 
 const getUser = (request, response) => {
-  const parsedUrl = url.parse(request.url);
-  const id = getId(parsedUrl.path);
-
-
-
-  response.writeHead(200, {"Content-Type": "application/json"});
-  response.write(JSON.stringify({ userId: id }));
-  response.end();
+const users = userBase();
+    if(users){
+        response.writeHead(200, {"Content-Type": "application/json"});
+        response.write(JSON.stringify(users));
+        response.end();
+    } else {
+        response.writeHead(500, { "Content-Type": "text/plain; charset=utf-8" });
+        response.write('Server error');
+        response.end();
+    }
+    
 };
-
 module.exports = getUser;
+
+
